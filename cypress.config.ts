378e07b76827c46d7d614cded69bbc16116316b7
underscoreDev <godswill@zimmerman.team>
2022-07-14 01:55:59 +0100

    feat: added eslint ignore script and cypress test script

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,7 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 });
